bcfe174c5862ffc787f1ddf45d2499f288183268
Nic Szer <nicszerman@gmail.com>
2017-09-23 13:13:41 -0400

    WIP time_suggester

--- block.py
+++ block.py
@@ -8,10 +8,11 @@ class Block:
         self.organizing_data = {
             'priority': 1, # possible default priority
             'location': (0, 0, 0), # gps coordinates
-            'purpose': '' # same as self.purpose. same purpose can
+            'purpose': '', # same as self.purpose. same purpose can
             # be considered same activity and be used to find patterns
-            'blocks_rejected': [] # in these blocks he is not available
-            
+            'blocks_rejected': [], # in these blocks he is not available
+            'time_ranges_convenience': [] # each element is 3-element tuple,
+            # first two elements are start and end, third is convenience
         }
 
     def get_purpose(self):
--- brain.py
+++ brain.py
@@ -30,6 +30,40 @@ def parse_request(request_text):
 
     return action
 
-# schedule is a Schedule object, time_now is an array with day and block#
+# schedule is a Schedule object, time_now is a tuple: (day, block#)
 def suggest_time(schedule, time_now, organizing_data):
+    # the basic first version of the time suggestion function calculates a
+    # how much match there is between block properties and organizing data
+    # properties, and adds them to an eponential decay function which tries
+    # to fill fast the empty blocks depending on how close they are to today
+    current_day, current_block = time_now
+
+    # iterate through schedule
+    day_number = 0
+    for block_list in schedule:
+        if day_number < current_day:
+            # check only for days in the future
+            day_number += 1
+            continue
+        block_number = 0
+        for block in day:
+            if day_number == current_day and block_number < current_block:
+                # check only for blocks in the future
+                block_number += 1
+                continue
+            # calculate exponential term of convenience index
+            total_block_distance = 0
+            total_block_distance += 24 * (day_number - current_day)
+            # day_number >= day_index
+            total_block_distance += (block_number - current_block)
+            # if block_number < current_block, blocks
+            # would be substracted, _which is correct_
+
+            # calculate exponential term with the block_distance (each block = 1hr)
+            # WIP TODO
+
+            # at the end of the for loop
+            block_number += 1
+        # at the end of the for loop
+        day_number += 1
     print('I suggest 5')
