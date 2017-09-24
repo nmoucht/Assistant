fa08197593a30ac5480113ba963e56c020a83c8d
nmoucht <n.mouchtaris@aol.com>
2017-09-24 01:54:04 -0400

    Update launch_website.py

--- launch_website.py
+++ launch_website.py
@@ -4,7 +4,17 @@ import json
 from flask.ext.pymongo import PyMongo
 from bson.objectid import ObjectId
 from pymongo import MongoClient
-import pymongo
+from bson.objectid import ObjectId
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+from signal import signal, SIGPIPE, SIG_DFL
+
+import datetime
 
 app = Flask(__name__)
 mongo=PyMongo(app)
@@ -13,6 +23,16 @@ connection = MongoClient("mongodb://ds147544.mlab.com:47544/")
 db = connection["userdatabase"]
 users=db.users
 db.authenticate(name="nikosm",password="Netherlands1")
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/calendar-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Calendar API Python Quickstart'
 @app.route('/')
 def main():
     return render_template('index.html')
